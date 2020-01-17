f392d56d05a9f1b5ee216f1cbd16176bdfa974a9
Stewart Young <stewartyoung97@gmail.com>
2020-01-17 13:08:56 +0000

    add blog style to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Layout from "../components/layout";
+import PostLink from "../components/postLink";
 
-const Index = () => (
+const Blog = () => (
     <Layout>
-        <p>Welcome to the home page</p>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink title="Hello Next.js"/>
+            <PostLink title="Backend programming with Next.js"/>
+            <PostLink title="Deploy apps with Zeit"/>
+        </ul>
     </Layout>
 );
 
-export default Index;
+export default Blog;
